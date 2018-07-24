a702a4857ff3acb5f8121abbcd805a9a32ea4b8b
Haris Ainur Rozak <harisrozak@gmail.com>
2018-07-24 09:45:45 +0700

    add nonce

--- wp-sample-admin-page.php
+++ wp-sample-admin-page.php
@@ -10,9 +10,13 @@ Contributor: Haris Ainur Rozak
 
 add_action( 'admin_action_wpse10500', 'wpse10500_admin_action' );
 function wpse10500_admin_action() {
-    // Do your stuff here
+	// check nonce
+	check_admin_referer('admin-page-wpse10500');
+
+	// do whatever you want
     update_option('example-data', sanitize_text_field($_POST['data']));
 
+    // redirect
     wp_redirect(add_query_arg( 'updated', '1', $_SERVER['HTTP_REFERER'] ));
     exit();
 }
@@ -36,9 +40,12 @@ function wpse10500_do_page() {
 		<?php endif ?>
 
 		<p>Your saved post data: <strong><?php echo $data ?></strong></p>
-		<form method="POST" action="<?php echo admin_url( 'admin.php' ); ?>">
+		<form method="POST" action="<?php echo admin_url('admin.php'); ?>">
 		    <!-- key for admin_action -->
 		    <input type="hidden" name="action" value="wpse10500" />
+
+		    <!-- nonce -->
+		    <?php wp_nonce_field('admin-page-wpse10500'); ?>
 		    
 		    <!-- your form -->
 		    <input type="text" name="data" value="<?php echo $data ?>" /><br><br>
